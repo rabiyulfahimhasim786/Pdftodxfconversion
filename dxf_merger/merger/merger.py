c42014b7c7a6c2c98d9fd5615643d2facadc8e70
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2023-11-23 04:50:28 -0800

    Add files via upload

---
+++ dxf_merger/merger/merger.py
@@ -0,0 +1,67 @@
+import sys
+import ezdxf
+from ezdxf.addons import Importer
+import json
+from os import listdir
+from os.path import isfile, join
+
+"""
+The idea of this script is to merge n dxf files (from pathin) into a target dxf file (into pathout)
+"""
+
+
+def merge(source, target):
+    """
+    input: source file (with dxf data), target data (empty shell where you add dxf data from source)
+    output: accumulative dxf data on target file
+    """
+    try:
+        # https://ezdxf.readthedocs.io/en/stable/drawing/management.html?highlight=readfile#ezdxf.readfile
+        source = ezdxf.readfile(source)
+
+    except ezdxf.DXFError as e:
+        print("\n" + "*" * 40)
+        print("FOUND DXF ERROR: {}".format(str(e)))
+        print("*" * 40 + "\n")
+        return False
+
+    try:
+        importer = Importer(source, target)
+        # import all entities from source modelspace into target modelspace
+        importer.import_modelspace()
+        # import all required resources and dependencies
+        importer.finalize()
+
+    except Exception as e:
+        print("Error -> ", e.__class__)
+
+
+# opening JSON file
+jsondata = json.load(open("data.json"))
+
+if jsondata["use_filelist"]:
+    # it use the hardcode file list from data.json
+    files = jsondata["filelist"]
+else:
+    # alternative way of get filelist only using pathin (no filename hardcoding), only dxf files
+    files = [
+        f
+        for f in listdir(jsondata["pathin"])
+        if isfile(join(jsondata["pathin"], f))
+        if f.lower().endswith(".dxf")
+    ]
+
+# empty target dxf
+target = ezdxf.new()
+
+# merger with filelist
+for file in files:
+    print("filein ->", file)
+    merge(jsondata["pathin"] + file, target)
+
+# save merged dfx target
+try:
+    print("target ->", jsondata["targetfile"])
+    target.saveas(jsondata["pathout"] + jsondata["targetfile"])
+except Exception as e:
+    print("Error -> ", e.__class__)
